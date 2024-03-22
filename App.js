d75ed6da1fcdcfa3d2af5ee34e20de2022ffa168
Benjamin EPS <benjamin_eps@hotmail.fr>
2024-03-22 14:43:38 +0100

    Création des composants et chargement  des questions sur Firestore

--- App.js
+++ App.js
@@ -1,20 +1,22 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import 'react-native-gesture-handler';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+
+import QuizApp from './src'; // importe composant QuizApp depuis le fichier index.js du dossier src
+import Playground from './src/Playground'; // importe composant Playground depuis le fichier Playground.js
+
+const Stack = createStackNavigator();
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      {/* Pile de navigation */}
+      <Stack.Navigator> 
+         {/* Première route avec le composant QuizApp */}
+        <Stack.Screen name="QuizApp" component={QuizApp} />
+        {/* Deuxième route avec le composant Playground */}
+        <Stack.Screen name="Playground" component={Playground} />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
